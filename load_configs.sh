e6489676abb9d446fcda97086266607ae721495d
Kacper Soczko <kacper.soczko@gmail.com>
2024-10-01 21:30:20 +0200

    update load_config

--- load_configs.sh
+++ load_configs.sh
@@ -12,10 +12,6 @@ echo "Installing all dependencies"
 sudo pacman -S autorandr kitty rofi neovim xclip xdotool
 
 echo "Dependencides installed, copying config files"
-
-mkdir -p /usr/share/rofi/themes
-cp rofi/base16-gruvbox-material-dark-medium.rasi /usr/share/rofi/themes/base16-gruvbox-material-dark-medium.rasi
-
 cp . ~/.config/
 
 echo "Done"
