8cd605f4d1834a37aca599b57547e9f9ee465345
Kacper Soczko <kacper.soczko@gmail.com>
2024-10-01 21:00:59 +0200

    reworked load script

--- load_configs.sh
+++ load_configs.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
 read -p "Are you sure? All config files will be overwritten" -n 1 -r
-echo    # (optional) move to a new line
+echo
+
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     exit 1
 fi
 
+echo "Installing all dependencies"
+sudo pacman -S autorandr kitty rofi neovim xclip xdotool
+
+echo "Dependencides installed, copying config files"
 
 mkdir -p /usr/share/rofi/themes
 cp rofi/base16-gruvbox-material-dark-medium.rasi /usr/share/rofi/themes/base16-gruvbox-material-dark-medium.rasi
 
 cp . ~/.config/
+
+echo "Done"
