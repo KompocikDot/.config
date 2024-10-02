939d055dd68bca8086ff693c6874b0966253aa73
Kacper Soczko <58148956+KompocikDot@users.noreply.github.com>
2024-10-02 08:10:13 +0200

    Update toggleterm config

--- nvim/lua/custom/plugins/toggleterm.lua
+++ nvim/lua/custom/plugins/toggleterm.lua
@@ -1,6 +1,15 @@
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
+  keys = {
+    { '<leader>t', '<cmd>ToggleTerm<cr>', desc = 'Toggle Terminal' },
+  },
+
+  opts = {
+    open_mapping = [[<c- >]],
+    insert_mappings = false,
+    terminal_mappings = true,
+  },
 
   config = function()
     require('toggleterm').setup()
