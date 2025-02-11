10d1d840ea315424fe7be3e9a5172ed6b8b3ef23
Kacper Soczko <kacper.soczko@gmail.com>
2025-02-11 07:18:57 +0100

    Add lazygit and fix statusbar

--- nvim/init.lua
+++ nvim/init.lua
@@ -448,12 +448,13 @@ require('lazy').setup({
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
-      require('mini.statusline').setup {
-        use_icons = vim.g.have_nerd_font,
-        section_location = function()
-          return '%2l:%-2v'
-        end,
-      }
+
+      local statusline = require 'mini.statusline'
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
     end,
   },
   {
